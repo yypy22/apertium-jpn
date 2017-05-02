ea9ac4765950dc2dc64fb576e455d9c3dfe50ac5
Darbus <doldham1@swarthmore.edu>
2017-05-01 22:51:36 -0400

    added digits

--- tests/eval.sh
+++ tests/eval.sh
@@ -1,1 +1,1 @@
-aq-covtest ~/Desktop/jpn-corpus/Hiroshima_Peace_Park_Wikipedia.txt ~/Source/apertium-jpn/jpn.automorf.bin
+aq-covtest ~/Desktop/jpn-corpus/combined_corpus.txt ~/Source/apertium-jpn/jpn.automorf.bin
