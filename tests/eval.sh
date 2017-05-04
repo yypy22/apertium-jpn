221ea136ebc9063c36f605ca4cf6cad6eba91b71
Darbus <doldham1@swarthmore.edu>
2017-05-04 17:06:46 -0400

    worked on morphology

--- tests/eval.sh
+++ tests/eval.sh
@@ -1,1 +1,5 @@
+aq-covtest ~/Desktop/jpn-corpus/Hiroshima_Peace_Park_Wikipedia.txt ~/Source/apertium-jpn/jpn.automorf.bin
+aq-covtest ~/Desktop/jpn-corpus/Grapefruit_Wikipedia.txt ~/Source/apertium-jpn/jpn.automorf.bin
+aq-covtest ~/Desktop/jpn-corpus/Shibori_Wikipedia.txt ~/Source/apertium-jpn/jpn.automorf.bin
+aq-covtest ~/Desktop/jpn-corpus/HG.txt ~/Source/apertium-jpn/jpn.automorf.bin
 aq-covtest ~/Desktop/jpn-corpus/combined_corpus.txt ~/Source/apertium-jpn/jpn.automorf.bin
